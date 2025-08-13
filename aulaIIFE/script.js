06bde9e35ab1017ab32bdb91e53ad1af30676422
Victor Borges <victorborgesoliveira22@gmail.com>
2025-08-13 17:23:17 -0300

    aprendendo sobre IIFE, funcoes invocadas automaticamente

---
+++ aulaIIFE/script.js
@@ -0,0 +1,14 @@
+// IIFE -> Immediately Invoked Function Expression, Funcoes imediatamente invocadas;
+
+(function(){
+    const nome = 'Victor'
+
+    console.log(nome);
+})();
+
+const nome = 'Rayssa'
+console.log(nome);
+
+// Sua sintaxe é composta por parenteses, uma funcao anonima e parenteses novamente apos os primeiros parenteses.
+// Essa funcao já é chamada e funcionada logo apos sua declaracao;
+// Ela tambem aceita parametros
